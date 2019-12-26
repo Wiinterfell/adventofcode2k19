3387c5285725e9ff02e129eac5ee1b5a9611142b
Sophie Legras <slegras@hubspot.com>
2019-12-26 18:11:59 +0100

    More clean up

--- day5.py
+++ day5.py
@@ -8,23 +8,17 @@ with open("input5.txt") as fp:
 
 	index = 0
 	while(instr[index] != 99):
-		nb_str = str(instr[index])
-		opcode = nb_str[-1]
-
+		opcode = instr[index] % 100
 		# get modes for parameters
-		firstParamMode = 0
-		if (len(nb_str) > 2):
-			firstParamMode = int(nb_str[-3])
-		secondParamMode = 0
-		if (len(nb_str) > 3):
-			secondParamMode = int(nb_str[-4])	
+		firstParamMode = instr[index] / 100 % 10
+		secondParamMode = instr[index] / 1000 % 10
 
 		# get parameters according to the modes
 		if (firstParamMode == 0):
 			firstNb = instr[instr[index+1]]
 		else:
 			firstNb = instr[index+1]
-		if (opcode in ("1", "2")):
+		if (opcode in (1, 2)):
 			if (secondParamMode == 0):
 				secondNb = instr[instr[index+2]]
 			else:
@@ -33,22 +27,22 @@ with open("input5.txt") as fp:
 
 		# compute result of instruction
 		res = 0
-		if opcode == "1":
+		if opcode == 1:
 			res = firstNb + secondNb
 			instr[res_position] = res
 			index += 4
-		elif opcode == "2":
+		elif opcode == 2:
 			res = firstNb * secondNb
 			instr[res_position] = res
 			index += 4
-		elif opcode == "3":
+		elif opcode == 3:
 			instr[instr[index+1]] = userInput
 			index += 2
-		elif opcode == "4":
+		elif opcode == 4:
 			if (firstNb != 0):
 				print(firstNb)
 				break
 			index += 2
 		else:
-			print("error " + opcode)
+			print("error " + str(opcode))
 			break
