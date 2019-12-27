5ab0413ed373316da6235594da2c01851981cee9
Sophie Legras <slegras@hubspot.com>
2019-12-27 11:05:13 +0100

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -10,6 +10,25 @@ def countForPlanet(tree, origin, planet, depth):
 		count += countForPlanet(tree, satellite, planet, depth+1)
 	return count
 
+def pathForPlanet(tree, origin, planet, path):
+	if (origin == planet):
+		return path
+	if not(origin in tree):
+		return []
+	for satellite in tree[origin]:
+		newPath = list(path)
+		newPath.append(satellite)
+		resultPath = pathForPlanet(tree, satellite, planet, newPath)
+		if (len(resultPath) > 0):
+			return resultPath
+	return []
+
+def findClosestCommonCenter(youPath, sanPath):
+	i = 0
+	while(youPath[i] == sanPath[i]):
+		i += 1
+	return youPath[i-1]
+
 
 tree = {}
 allPlanets = set()
@@ -27,7 +46,9 @@ with open("input6.txt") as fp:
 		else:
 			tree[center] = [orbiting]
 
-count = 0
-for planet in allPlanets:
-	count += countForPlanet(tree, "COM", planet, 0)
-print(count)
+youPath = pathForPlanet(tree, "COM", "YOU", [])
+sanPath = pathForPlanet(tree, "COM", "SAN", [])
+closestCommonCenter =findClosestCommonCenter(youPath, sanPath)
+distance = countForPlanet(tree, closestCommonCenter, "YOU", 0) + countForPlanet(tree, closestCommonCenter, "SAN", 0) - 2 # SAN and YOU aren't orbit transfers
+print(distance)
+
